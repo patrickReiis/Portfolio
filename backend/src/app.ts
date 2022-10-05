99232b716dd09e367183e4ea004e6cc0382a0de7
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2022-10-05 11:16:34 -0300

    Build files and endpoint with dummy data

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -15,15 +15,58 @@ const server = http.createServer((req:IncomingMessage, res:ServerResponse) =>{
 
     if (req.method === 'GET'){
 
-        if (req.url === '/api/v1/test'){
+        if (req.url === '/api/v1/projects'){
             res.writeHead(200, {'Content-Type':'application/json'});
-            res.end(JSON.stringify({'success':'up and working'}));
+            res.end(JSON.stringify([                                          
+                {                                                                                  
+                    "title": "Calculator with 3D graphics",                                        
+                    "content": `It was quite hard wasn't <img src="https://i.imgur.com/WlIyo8Z.png" alt="Picture of anime">`,
+                        "categories": ['typeorm', 'vuejs', 'python'],                              
+                    "brief":'Application to play tictactoe that allows bla bla',                   
+                    "video": 'https://www.youtube.com/watch?v=9gXtkG8jOHY',                        
+                    "id": 1,                                                                       
+                    lang:'pt-br'
+                },                                                                                                                                                                               
+                { 
+                    "title": "Robot mecho",
+                    "content": `It was quite hard wasn't <img src="https://i.imgur.com/WlIyo8Z.png"alt="Picture of anime">`,
+                        "categories": ['typeorm', 'vuejs', 'python'],
+                    "brief":'Game that has robots and stuff allows bla bla',
+                    "video": 'https://www.youtube.com/watch?v=9gXtkG8jOHY',
+                    lang:'en-us',
+                    id:2
+                }
+            ]));
             return
         }
 
+        else if( /^\/api\/v1\/project\/[0-9]+$/.test(req.url as string) ){
+            const basePath = '/api/v1/project/'
+            const projectId = (req.url as string).slice(basePath.length);
+           
+            res.writeHead(200, {'Content-Type':'application/json'});
+            res.end(JSON.stringify({                                                                                  
+                    "title": "Calculator with 3D graphics",                                        
+                    "content": `It was quite hard wasn't <img src="https://i.imgur.com/WlIyo8Z.png" alt="Picture of anime">`,
+                    "categories": ['typeorm', 'vuejs', 'python'],                              
+                    "brief":'Application to play tictactoe that allows bla bla',                   
+                    "video": 'https://www.youtube.com/watch?v=9gXtkG8jOHY',                        
+                    "id": 1,                                                                       
+                    lang:'pt-br'
+                }));
+            
+            return
+        }
         else {
             fs.readFile(filePath, (err:NodeJS.ErrnoException, data:Buffer)=>{
-                if (err) throw err;
+                if (err) {
+                    if (err.code === 'ENOENT'){
+                        res.writeHead(200 , {'Content-Type':'application/json'})
+                        res.end('Not found')
+                        return
+                    }
+                    throw err;
+                }
 
                 res.writeHead(200, {'Content-Type':contentType});
                 res.end(data)
