c66bea7903448bed50b86c9cc26cea44af9ade4b
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2022-10-13 10:39:24 -0300

    Changed build path and removed proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,15 +12,6 @@ export default defineConfig({
     }
   },
   build: {
-      outDir: '../backend/src/frontend'
-  },
-  server: {
-      proxy: {
-          '/api': {
-              target: 'http://localhost:3000/',       
-              changeOrigin: true,
-              secure: false
-          }
-      }
+      outDir: 'public'
   }
 })
