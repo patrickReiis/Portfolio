e1bca2eb11d6ee812e78ef118c85e042fded6f5c
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2023-01-16 22:24:56 -0300

    Division component done

---
+++ frontend/src/Division.vue
@@ -0,0 +1,35 @@
+<template> 
+
+<div class="rule-container">
+  <div class="rule rule-1"></div>
+  <hr>
+  <div class="rule rule-2"></div>
+  <hr>
+  <div class="rule rule-3"></div>
+  <hr>
+</div>
+</template>
+
+<style scoped> 
+.rule-container {
+  margin: 1rem auto;
+}
+
+hr {
+    display: none;
+}
+
+div.rule {
+    height: 5px;
+}
+
+.rule-1, .rule-3 {
+  background: #A6A6A6;
+    height: 5px;
+}
+
+.rule-2 {
+  background: #EEEEE6;
+  height: 5px;
+}
+</style>
