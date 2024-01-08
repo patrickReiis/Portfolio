edf2302196ba14c92df44f9f06230b79f72be99b
P. Reis <76563803+patrickReiis@users.noreply.github.com>
2024-01-07 23:50:45 -0300

    fix: indentation

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,87 +1,97 @@
 <script lang="ts" setup>
 // This is the 'mother' component, it's job is to provide the routes and components based on the user's chosen language
-import { ref, computed } from 'vue';
-import DetailedProjectVue from './DetailedProject.vue';
-import Home from './Home.vue';
-import NotFound from './NotFound.vue';
-import HomePt from './HomePt.vue';
+import { ref, computed } from "vue";
+import DetailedProjectVue from "./DetailedProject.vue";
+import Home from "./Home.vue";
+import NotFound from "./NotFound.vue";
+import HomePt from "./HomePt.vue";
 
-const routes:any = {'/home': Home, '/home-pt': HomePt}
+const routes: any = { "/home": Home, "/home-pt": HomePt };
 
-const  currentPath = ref(window.location.hash)
+const currentPath = ref(window.location.hash);
 
-const currentLangSelected = ref('en');
+const currentLangSelected = ref("en");
 
-window.addEventListener('hashchange', () => {
-   currentPath.value = window.location.hash; 
-})
+window.addEventListener("hashchange", () => {
+  currentPath.value = window.location.hash;
+});
 
 const currentView = computed(() => {
+  if (/^\/home(-pt)?\/[0-9]+$/.test(currentPath.value.slice(1))) {
+    // project-<any-number>, for example: project-2
+    return DetailedProjectVue;
+  }
 
-    if( /^\/home(-pt)?\/[0-9]+$/.test(currentPath.value.slice(1)) ){ // project-<any-number>, for example: project-2
-       return DetailedProjectVue; 
-    }
-
-    return routes[currentPath.value.slice(1) || '/home'] || NotFound // Be not found in the future 
+  return routes[currentPath.value.slice(1) || "/home"] || NotFound; // Be not found in the future
 });
 
 function goToHome() {
-console.log(window.location.hash.slice(1, '/home-pt'.length + 1) === '/home-pt');
-    window.location.hash = window.location.hash.slice(1, '/home-pt'.length + 1) === '/home-pt' ? '/home-pt' : '/home' 
-     
+  console.log(
+    window.location.hash.slice(1, "/home-pt".length + 1) === "/home-pt"
+  );
+  window.location.hash =
+    window.location.hash.slice(1, "/home-pt".length + 1) === "/home-pt"
+      ? "/home-pt"
+      : "/home";
 }
 
 goToHome();
-
 </script>
 
 <template>
-
-    <nav class="nav-app">
+  <nav class="nav-app">
     <div class="my-name-container">
-       <h1 id="patrick" class="my-name" @click="goToHome">
-          PATRICK 
-      </h1>
-       <h1 class="my-name" @click="goToHome">
-        DOS REIS 
-        </h1>
+      <h1 id="patrick" class="my-name" @click="goToHome">PATRICK</h1>
+      <h1 class="my-name" @click="goToHome">DOS REIS</h1>
     </div>
     <div class="language-container">
-            <ul class="lang-ul">
-                <li><a  :class="currentLangSelected  === 'en' ? 'selected' : ''" @click="currentLangSelected = 'en'" href="#/home">English</a></li>
-                <li><a  :class="currentLangSelected  === 'pt' ? 'selected' : ''" @click="currentLangSelected = 'pt'" href="#/home-pt">Português</a></li>
-            </ul>
-        </div>
-    </nav>
-     
-    <component :is="currentView" />
+      <ul class="lang-ul">
+        <li>
+          <a
+            :class="currentLangSelected === 'en' ? 'selected' : ''"
+            @click="currentLangSelected = 'en'"
+            href="#/home"
+            >English</a
+          >
+        </li>
+        <li>
+          <a
+            :class="currentLangSelected === 'pt' ? 'selected' : ''"
+            @click="currentLangSelected = 'pt'"
+            href="#/home-pt"
+            >Português</a
+          >
+        </li>
+      </ul>
+    </div>
+  </nav>
 
+  <component :is="currentView" />
 </template>
 
 <style scoped>
-
 .language-container {
-    display: flex;
-    justify-content: end;
-    align-items: center;
-    z-index: 1;
-position: absolute;
-right: 0;
-top: 0;
-padding: 0.5rem;
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  z-index: 1;
+  position: absolute;
+  right: 0;
+  top: 0;
+  padding: 0.5rem;
 }
 
 .selected {
-  background: #A6A6A6;
-padding: 0.4rem;
-border-radius: 1rem;
+  background: #a6a6a6;
+  padding: 0.4rem;
+  border-radius: 1rem;
 }
 
 .world-icon {
-    width: 25px;
-    height: 25px;
-    fill: var(--main-font-color);
-    margin: 0 0.5rem;
+  width: 25px;
+  height: 25px;
+  fill: var(--main-font-color);
+  margin: 0 0.5rem;
 }
 
 #patrick {
@@ -89,36 +99,36 @@ border-radius: 1rem;
 }
 
 .my-name {
-    cursor: pointer;
-    color: var(--main-font-color);
-    font-size: 5rem;
-    font-family: mainFont;
-    line-height: 1;
-width: fit-content;
-margin: auto;
+  cursor: pointer;
+  color: var(--main-font-color);
+  font-size: 5rem;
+  font-family: mainFont;
+  line-height: 1;
+  width: fit-content;
+  margin: auto;
 }
 
 .nav-app {
-    position: sticky;
-    top: 0;
-    z-index: 1;
-    padding-top: 1rem;
-background: var(--background-color);
+  position: sticky;
+  top: 0;
+  z-index: 1;
+  padding-top: 1rem;
+  background: var(--background-color);
 }
 
 .lang-ul {
-    list-style:none;
-    text-align: end;
-    padding: 0;
+  list-style: none;
+  text-align: end;
+  padding: 0;
 }
 
 .lang-ul li {
-margin: 0.1rem;
+  margin: 0.1rem;
 }
 
 .lang-ul li a {
-    color: var(--main-font-color);
-    text-decoration: none;
+  color: var(--main-font-color);
+  text-decoration: none;
 }
 
 @media screen and (min-width: 0px) and (max-width: 700px) {
@@ -132,8 +142,6 @@ margin: 0.1rem;
 
   .my-name-container {
     margin-top: 3rem;
+  }
 }
-
-}
-
 </style>
