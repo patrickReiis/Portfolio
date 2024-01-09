384861cf00183985c6a6ff2832ae0f0019237511
P. Reis <76563803+patrickReiis@users.noreply.github.com>
2024-01-09 13:27:14 -0300

    feat: 'read more' about project without going to a new page

--- frontend/src/ProjectFooter.vue
+++ frontend/src/ProjectFooter.vue
@@ -1,30 +1,43 @@
 <script lang="ts" setup>
+import { ref } from "vue";
+
 defineProps({
   technologies: Array,
-  projectId: Number
+  projectId: Number,
+  projectContent: String,
 });
 
-function handleShowProject(id: number) {
-  const currentURL = window.location.hash
-  return currentURL + `/${id+1}`
-}
+const toggleReadMore = ref(false);
 
+function handleShowReadMore() {
+  const currentURL = window.location.hash;
+  return currentURL;
+}
 </script>
 
 <template>
-            <div class="tech-container">
-                <p>
-                    Technologies Used:
-                </p>
-                <ul class="project-technologies">
-                    <li v-for="tech in technologies" :key="tech as string" class="item-tech">
-                            {{tech}}
-                        </li>
-                    </ul>
-                </div>
-            <div class="read-more-container">
-                <a class="read-more" v-bind:href="handleShowProject(projectId as number)">
-                    Read Entire Project
-                </a>
-            </div>
+  <div class="tech-container">
+    <p>Technologies Used:</p>
+    <ul class="project-technologies">
+      <li v-for="tech in technologies" :key="tech as string" class="item-tech">
+        {{ tech }}
+      </li>
+    </ul>
+  </div>
+  <div class="project-footer-content-container">
+    <div
+      v-if="toggleReadMore"
+      class="project-footer-content"
+      v-html="projectContent"
+    ></div>
+  </div>
+  <div class="read-more-container">
+    <a
+      class="read-more"
+      @click="toggleReadMore = !toggleReadMore"
+      v-bind:href="handleShowReadMore()"
+    >
+      {{ toggleReadMore == false ? "Read More" : "Hide" }}
+    </a>
+  </div>
 </template>
